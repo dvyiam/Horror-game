4bb974d8038ba1c54f657467849fa2ec6dbe7c4f
Divyam Khare <divyamkhare12@gmail.com>
2023-04-10 11:33:48 +0530

    enemy ai added

--- Horror game/Assets/Scripts/Enemy.cs
+++ Horror game/Assets/Scripts/Enemy.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] Transform player;
+    public int speed = 5;
+    public int minDis = 4;
+    public int maxDis = 10;
+    NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = 8f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(player);
+
+        if(Vector3.Distance(transform.position, player.position) > minDis)
+        {
+            agent.destination = player.position;
+        }
     }
 }
